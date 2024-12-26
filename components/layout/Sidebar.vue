7c0d8c335b59ff7ed8925997834cde28a5ad215e
WarGhost132 <wawghostick@gmail.com>
2024-12-26 11:46:32 +0300

    add nuxt icon

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -9,6 +9,8 @@
       <NuxtImg src="/logo.svg" alt="" width="100px" class="mx-auto" />
     </NuxtLink>
 
-    <UiButton>Send</UiButton>
+    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+      <Icon name="line-md:logout" size="20" />
+    </button>
   </aside>
 </template>
