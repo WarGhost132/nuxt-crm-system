856d3d79549bfdc5c7a0060a7e2016caeee5403f
WarGhost132 <wawghostick@gmail.com>
2024-12-26 11:02:37 +0300

    minor fix

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -4,7 +4,7 @@
 
 <template>
   <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
-    <!-- TODO: Add shadecn -->
+    
     <NuxtLink to="/" class="mb-10 block">
       <NuxtImg src="/logo.svg" alt="" width="100px" class="mx-auto" />
     </NuxtLink>
