b22d688c5b6a74d317841a88320847ab48461463
WarGhost132 <wawghostick@gmail.com>
2024-12-27 11:00:54 +0300

    implement auth

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,8 +1,22 @@
 <script lang="ts" setup>
+import { account } from '~/lib/appwrite';
+
+
+const authStore = useAuthStore()
+const router = useRouter()
+const isLoadingStore = useIsLoadingStore()
+
+const logout = async () => {
+  isLoadingStore.set(true)
+
+  await account.deleteSession('current')
+  authStore.clear()
+  await router.push('/login')
+
+  isLoadingStore.set(false)
+}
 
 </script>
-
-<!-- TODO: Auth -->
 
 <template>
   <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
@@ -11,7 +25,7 @@
       <NuxtImg src="/logo.svg" alt="" width="140px" class="mx-auto" />
     </NuxtLink>
 
-    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+    <button class="absolute top-2 right-3 transition-colors hover:text-primary" @click="logout">
       <Icon name="line-md:logout" size="20" />
     </button>
 
--- pages/login.vue
+++ pages/login.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { account } from '~/lib/appwrite'
+import { v4 as uuid } from 'uuid'
 
 useSeoMeta({
   title: 'Login | CRM System'
@@ -9,9 +11,38 @@ const passwordRef = ref('')
 const nameRef = ref('')
 
 const isLoadingStore = useIsLoadingStore()
+const authStore = useAuthStore()
 const router = useRouter()
 
-// TODO: Login and register
+const login = async () => {
+  isLoadingStore.set(true)
+
+  await account.createEmailPasswordSession(emailRef.value, passwordRef.value)
+
+  const response = await account.get()
+
+  if (response) {
+    authStore.set({
+      email: response.email,
+      name: response.name,
+      status: response.status
+    })
+  }
+
+  emailRef.value = ''
+  passwordRef.value = ''
+  nameRef.value = ''
+
+  await router.push('/')
+
+  isLoadingStore.set(false)
+}
+
+const register = async () => {
+  await account.create(uuid(), emailRef.value, passwordRef.value, nameRef.value)
+
+  await login()
+}
 
 </script>
 
@@ -26,8 +57,8 @@ const router = useRouter()
         <UiInput v-model="nameRef" placeholder='Name' type="text" class="mb-3" />
 
         <div class="flex items-center justify-center gap-5">
-          <UiButton type="button">Login</UiButton>
-          <UiButton type="button">Register</UiButton>
+          <UiButton type="button" @click="login">Login</UiButton>
+          <UiButton type="button" @click="register">Register</UiButton>
         </div>
       </form>
     </div>
