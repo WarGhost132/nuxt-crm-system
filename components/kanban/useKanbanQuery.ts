6bdeb54737208dbc72efd83240fe944db4916859
WarGhost132 <wawghostick@gmail.com>
2024-12-28 12:06:16 +0300

    transformation data in kanban

--- components/kanban/useKanbanQuery.ts
+++ components/kanban/useKanbanQuery.ts
@@ -6,11 +6,27 @@ import type { IDeal } from "~/types/deals.types";
 
 export function useKanbanQuery() {
   return useQuery({
-    queryKey: ["dealse"],
+    queryKey: ["deals"],
     queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS),
     select(data) {
       const newBoard = [...KANBAN_DATA];
       const deals = data.documents as unknown as IDeal[];
+
+      for (const deal of deals) {
+        const column = newBoard.find((col) => col.id === deal.status);
+        if (column) {
+          column.items.push({
+            $createdAt: deal.$createdAt,
+            id: deal.$id,
+            name: deal.name,
+            price: deal.price,
+            companyName: deal.customer.name,
+            status: column.name,
+          });
+        }
+      }
+
+      return newBoard;
     },
   });
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import type { ICard, IColumn } from '~/components/kanban/kanban.types';
+import { useKanbanQuery } from '~/components/kanban/useKanbanQuery';
+import { convertCurrency } from '~/lib/convertCurrency';
+import dayjs from 'dayjs'
 
 useSeoMeta({
   title: 'Home | CRM System'
@@ -7,20 +10,37 @@ useSeoMeta({
 
 const dragCardRef = ref<ICard | null>(null)
 const sourceColumnRef = ref<IColumn | null>(null)
-
-
+const {data, isLoading, refetch} = useKanbanQuery()
 </script>
 
 <template>
   <div class="p-10">
     <h1 class="font-bold text-2xl mb-10">CRM System by WarGhost</h1>
 
-    <div>
-      <UiCard class="mb-3" draggable="true">
-        <UiCardHeader role="button"> name card </UiCardHeader>
-        <UiCardContent> Компания </UiCardContent>
-        <UiCardFooter> Date </UiCardFooter>
-      </UiCard>
+    <div v-if="isLoading">Loading...</div>
+
+    <div v-else>
+      <div class="grid grid-cols-5 gap-16">
+        <div v-for="(column) in data" :key="column.id">
+          <div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center">
+            {{ column.name }}
+          </div>
+          <div>
+            <UiCard v-for="card in column.items" :key="card.id" class="mb-3" draggable="true">
+              <UiCardHeader role="button">
+                <UiCardTitle>
+                  {{ card.name }}
+                </UiCardTitle>
+                <UiCardDescription class="mt-2 block">
+                  {{ convertCurrency(card.price) }}
+                </UiCardDescription>
+              </UiCardHeader>
+              <UiCardContent class="text-xs"> Компания {{ card.companyName }} </UiCardContent>
+              <UiCardFooter> {{ dayjs(card.$createdAt).format('DD MMMM YYYY') }} </UiCardFooter>
+            </UiCard>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
