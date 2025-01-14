65d1995a74f23a840aec5473bc5b70138f75361e
WarGhost132 <wawghostick@gmail.com>
2025-01-14 14:58:13 +0300

    add comments

--- components/kanban/slideover/Comments.vue
+++ components/kanban/slideover/Comments.vue
@@ -1,7 +1,36 @@
 <script setup lang="ts">
-const store = useDealSlideStore()
+import { useComments } from './useComments'
+import { useCommentCreate } from './useCommentCreate'
+import type { IDeal } from '~/types/deals.types'
+import dayjs from 'dayjs'
+
+const { data, refetch, isLoading } = useComments()
+const { commentRef, writeComment } = useCommentCreate({ refetch })
+
+const card = data as unknown as IDeal
 </script>
 
 <template>
-  <div></div>
+  <UiInput
+    placeholder="Оставьте комментарий"
+    v-model="commentRef"
+    @keyup.enter="writeComment"
+  />
+
+  <UiSkeleton v-if="isLoading" class="w-full h-[76px] rounded mt-5" />
+  <div v-else-if="card">
+    <div
+      v-for="comment in card?.comments"
+      :key="comment.$id"
+      class="flex items-start mt-5"
+    >
+      <Icon name="radix-icons:chat-bubble" class="mr-3 mt-1" size="25" />
+      <div class="border-border bg-black/20 rounded p-3 w-full">
+        <div class="mb-2 text-sm">
+          Комментарий {{ dayjs(comment.$createdAt).format('HH:mm') }}
+        </div>
+        <p>{{ comment.text }}</p>
+      </div>
+    </div>
+  </div>
 </template>
