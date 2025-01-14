e7d1161b729147aed0ea8d3bfca130deeeb3c918
WarGhost132 <wawghostick@gmail.com>
2025-01-14 12:48:08 +0300

    used prettier again

--- components/ui/card/CardTitle.vue
+++ components/ui/card/CardTitle.vue
@@ -1,21 +1,18 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue';
-import { cn } from '@/lib/utils';
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
 
 const props = defineProps<{
-    class?: HTMLAttributes['class'];
-}>();
+  class?: HTMLAttributes['class']
+}>()
 </script>
 
 <template>
-    <h3
-        :class="
-            cn(
-                'text-xl font-semibold leading-none tracking-tighter',
-                props.class,
-            )
-        "
-    >
-        <slot />
-    </h3>
+  <h3
+    :class="
+      cn('text-xl font-semibold leading-none tracking-tighter', props.class)
+    "
+  >
+    <slot />
+  </h3>
 </template>
